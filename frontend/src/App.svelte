3d296c03000bc120f08d725f6d2e4d29557e98bd
Chaoscaot <max@chaoscaot.de>
2024-11-01 20:33:52 +0000

    Add Design

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,4 +2,55 @@
 import Converter from "./lib/Converter.svelte";
 </script>
 
-<Converter />
+<div>
+    <nav>
+        <span>Structure2Schematic</span>
+    </nav>
+
+    <Converter />
+
+    <footer>
+        <a href="https://github.com/Chaoscaot/Structure2Schematic">GitHub</a>
+    </footer>
+</div>
+
+<style>
+    footer {
+        display: flex;
+        width: 100vw;
+
+        border-top: 1px solid #1e1f22;
+        margin-top: 4rem;
+        padding-top: 1rem;
+
+        justify-content: center;
+        align-items: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: #6c6d73;
+    }
+
+    div {
+        height: 100vh;
+        width: 100vw;
+
+        background: black;
+        color: white;
+    }
+
+    nav {
+        width: 100vw;
+        height: 3rem;
+        display: flex;
+
+        >span {
+            font-weight: bold;
+            font-size: 1.5rem;
+
+            margin: auto;
+
+        }
+    }
+</style>
--- frontend/src/lib/Converter.svelte
+++ frontend/src/lib/Converter.svelte
@@ -2,8 +2,8 @@
     import { convert_structure } from 's2s_web'
     import saveAs from "file-saver"
 
-    function onFileChange(event: Event & { currentTarget: HTMLInputElement }) {
-        for (const file of event.currentTarget.files!) {
+    function convertFiles(files: FileList) {
+        for (const file of files) {
             const reader = new FileReader()
 
             reader.readAsArrayBuffer(file)
@@ -20,8 +20,45 @@
             }
         }
 
-        event.currentTarget.value = ''
+        input.value = ""
     }
+
+    let input: HTMLInputElement
 </script>
 
-<input type="file" multiple accept=".nbt,.mcstructure" onchange={onFileChange}>
+<label ondrop={(e) => {
+    e.preventDefault()
+    convertFiles(e.dataTransfer.files)
+}} ondragover={(e) => e.preventDefault()}>
+    Select .nbt or .mcstructure files to convert to .schem
+    <input bind:this={input} type="file" multiple accept=".nbt,.mcstructure" onchange={(e) => convertFiles(e.currentTarget.files)}>
+</label>
+
+<style>
+    label {
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        width: 80%;
+        height: 70%;
+
+        background: #1e1f22;
+        border-radius: 2rem;
+        margin: auto;
+
+    }
+
+    label:hover {
+        cursor: pointer;
+    }
+
+    label:focus-visible {
+        background: #2e2f32;
+    }
+
+    input {
+        display: none;
+    }
+</style>
